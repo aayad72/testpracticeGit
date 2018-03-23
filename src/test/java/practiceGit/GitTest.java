5c90f1b29e5cd846db3c2ff16394b028d77b358a
ikbol009@gmail.com <ikbol009@gmail.com>
2018-03-23 17:16:41 -0400

    dasdgadg

--- src/test/java/practiceGit/GitTest.java
+++ src/test/java/practiceGit/GitTest.java
@@ -10,23 +10,16 @@ public class GitTest {
 
 		
 		System.out.println("git practice");
-		
+		System.out.println("aaaaa");
 		
 		System.out.println("Bye from me");
 
 		System.out.println("Hi from me");
 		
-
-		for (String string : args) {
-			
-		}
-		
-		String  str= "qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqrt";
-		
-
 		System.out.println("No more excuse");
 		
 		System.out.println("java");
+		System.out.println("ffffff");
 
 
 	}
